1ee3b20a347f44792d8f1948742bdad3dc0a598c
ExponentialRadix <tauler.euler.e@gmail.com>
2026-01-17 00:36:30 +0100

    Actualizar post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,14 +1,9 @@
 #!/system/bin/sh
-MODPATH=${0%/*}
+# Nexus-KHF - Pre-Boot Tweaks
 
-# ZRAM 2.85GB LZ4
-echo lz4 > /sys/block/zram0/comp_algorithm
-echo 2885238784 > /sys/block/zram0/disksize
+# Desactivar el Thermal Core de Xiaomi (previene bajones de FPS por calor moderado)
+stop mi_thermald
+stop thermal-engine
 
-# Latencia de Kernel 1ms
-echo 1000000 > /proc/sys/kernel/sched_latency_ns
-
-# I/O Scheduler: Aplicar a todos los bloques disponibles
-for queue in /sys/block/*/queue/scheduler; do
-    [ -f "$queue" ] && echo "none" > "$queue" 2>/dev/null
-done
+# Eliminar límites de memoria de procesos pesados
+echo 0 > /sys/module/lowmemorykiller/parameters/enable_lmk
